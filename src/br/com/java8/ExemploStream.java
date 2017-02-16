9aa623cd3dd992a88c706f2039766ac69bd92a8b
sronLemos <sron.lemos@gmail.com>
2017-02-15 22:33:38 -0300

    Stream Java 8

---
+++ src/br/com/java8/ExemploStream.java
@@ -0,0 +1,56 @@
+package br.com.java8;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Predicate;
+
+class Curso {
+	private String nome;
+	private int alunos;
+
+	public Curso(String nome, int alunos) {
+		this.nome = nome;
+		this.alunos = alunos;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getAlunos() {
+		return alunos;
+	}
+
+	public void setAlunos(int alunos) {
+		this.alunos = alunos;
+	}
+
+}
+
+public class ExemploStream {
+	public static void main(String[] args) {
+		List<Curso> cursos = new ArrayList<>();
+		cursos.add(new Curso("Python", 45));
+		cursos.add(new Curso("JavaScript", 150));
+		cursos.add(new Curso("Java 8", 113));
+		cursos.add(new Curso("C", 55));
+
+		cursos.sort(Comparator.comparing(Curso::getAlunos));
+		// cursos.forEach(c -> System.out.println(c.getNome()));
+
+		cursos.stream()
+			.filter(c -> c.getAlunos() >= 100)
+			.forEach(c -> System.out.println(c.getNome()));
+
+		cursos.stream()
+			.filter(c -> c.getAlunos() >= 100)
+			.map(c -> c.getAlunos())
+			.forEach(total -> System.out.println(total));
+	}
+}
